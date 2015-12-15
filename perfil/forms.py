14259ddc2a529170cb5d2024117c6dd733e60e28
lizethvelasco <lalivelascom@gmail.com>
2015-12-15 16:38:21 -0500

    nueva rama

--- perfil/forms.py
+++ perfil/forms.py
@@ -6,10 +6,11 @@ class PostForm(forms.ModelForm):
 
 	class Meta:
 		model=User
+		
 		fields=('name','lastname','photo') #datos que se muestran en el formulario
 
-class ProjectForm(forms.ModelForm):
-	
+class ProjectForm(forms.ModelForm):   
+
 	class Meta:
 		model=Project
 		fields=('name','user')
